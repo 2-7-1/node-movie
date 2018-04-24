c11492ca3edbcab7dd68cc8a18914c58a1e7349c
2-7-1 <2-7-1>
2018-04-23 23:50:21 -0400

    -- Updated README.md -- One DB connection in movie.js

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Skeleton project for Swagger
+# node-movie
+This is a formal API using Swagger and SwaggerExpress demonstrating:
+-- Project code-gen templated with "swagger project create"
+-- swagger.yaml OpenAPI definition
+-- May be used with Swagger UI and Swagger Editor
+-- AWS RDS (MySQL) database for datastor
+
+# USE
+See https://github.com/2-7-1/node-movie/blob/master/api/swagger/swagger.yaml for API definition.
--- api/controllers/movie.js
+++ api/controllers/movie.js
@@ -18,30 +18,25 @@ var connection = mysql.createConnection({
     port: 3306,
     database: 'movie'
 });
+connection.connect();
 
 // GET all movies
 function getAll(req, res, next) {
-    connection.connect();
     connection.query('SELECT * FROM movie.movie', function (error, results, fields) {
         if (error) {
-            connection.end();
             throw error;
         }
-        connection.end();
         res.json(results);
     });
 }
 
 // POST movie
 function save(req, res, next) {
-    connection.connect();
     connection.query("INSERT INTO `movie`.`movie` (`title`, `year`) VALUES ('" + req.body.title + "', " + req.body.year + ")",
         function (error, results, fields) {
             if (error) {
-                connection.end();
                 throw error;
             }
-            connection.end();
             res.json({
                 success: 1,
                 description: "Movie added to list"
@@ -53,14 +48,11 @@ function save(req, res, next) {
 // GET /movie/{id}
 function getOne(req, res, next) {
     var title = req.swagger.params.id.value;
-    connection.connect();
     connection.query("SELECT * FROM `movie`.`movie` WHERE title = '" + title + "'",
         function (error, results, fields) {
             if (error) {
-                connection.end();
                 throw error;
             }
-            connection.end();
             res.json(results);
         }
     );
@@ -70,14 +62,11 @@ function getOne(req, res, next) {
 function update(req, res, next) {
     var title = req.swagger.params.id.value;
     var movie = req.body;
-    connection.connect();
     connection.query("UPDATE `movie`.`movie` SET title = '" + movie.title + "', " + " year = " + movie.year + " WHERE title = '" + title + "'",
         function (error, results, fields) {
             if (error) {
-                connection.end();
                 throw error;
             }
-            connection.end();
             res.json({
                 success: 1,
                 description: "Movie updated"
@@ -89,14 +78,11 @@ function update(req, res, next) {
 // DELETE /movie/{id}
 function deleteMovie(req, res, next) {
     var title = req.swagger.params.id.value;
-    connection.connect();
     connection.query("DELETE FROM `movie`.`movie` WHERE title ='" + title + "'",
         function (error, results, fields) {
             if (error) {
-                connection.end();
                 throw error;
             }
-            connection.end();
             res.json({
                 success: 1,
                 description: "Movie deleted"
